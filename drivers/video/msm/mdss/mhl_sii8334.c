73dc8be9060785fd26963732dedf0aef070df52f
raghavendra ambadas <rambad@codeaurora.org>
2014-09-08 13:58:22 +0530

    msm: mhl: sii8334: Fix null pointer dereference at power off
    Check for null pointer before disabling the regulators.
    During cable connect/disconnect and suspend/resume check for
    previous power state of mhl to avoid enabling/disabling gpio's
    and regulators twice.
    
    Change-Id: I44dc51122da4630974147b829236cb39c1ad54b0
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mdss/mhl_sii8334.c

--- drivers/video/msm/mdss/mhl_sii8334.c
+++ drivers/video/msm/mdss/mhl_sii8334.c
@@ -381,10 +381,30 @@ static int mhl_sii_wait_for_rgnd(struct mhl_tx_ctrl *mhl_ctrl)
 static int mhl_sii_device_discovery(void *data, int id,
 			     void (*usb_notify_cb)(void *, int), void *ctx)
 {
-	int rc;
-	struct mhl_tx_ctrl *mhl_ctrl = data;
-	struct i2c_client *client = mhl_ctrl->i2c_handle;
-	unsigned long flags;
+	int rc = 0;
+	struct i2c_client *client = NULL;
+
+	if (!mhl_ctrl) {
+		pr_err("%s: ctrl is NULL\n", __func__);
+		return -EINVAL;
+	}
+
+	client = mhl_ctrl->i2c_handle;
+
+	if (on && !mhl_ctrl->irq_req_done) {
+		rc = mhl_vreg_config(mhl_ctrl, 1);
+		if (rc) {
+			pr_err("%s: vreg init failed [%d]\n",
+				__func__, rc);
+			return -ENODEV;
+		}
+
+		rc = mhl_gpio_config(mhl_ctrl, 1);
+		if (rc) {
+			pr_err("%s: gpio init failed [%d]\n",
+				__func__, rc);
+			return -ENODEV;
+		}
 
 	if (!mhl_ctrl->irq_req_done) {
 		rc = request_threaded_irq(mhl_ctrl->i2c_handle->irq, NULL,
@@ -399,8 +419,11 @@ static int mhl_sii_device_discovery(void *data, int id,
 			pr_debug("request_threaded_irq succeeded\n");
 			mhl_ctrl->irq_req_done = true;
 		}
-	} else {
-		enable_irq(client->irq);
+	} else if (!on && mhl_ctrl->irq_req_done) {
+		free_irq(mhl_ctrl->i2c_handle->irq, mhl_ctrl);
+		mhl_gpio_config(mhl_ctrl, 0);
+		mhl_vreg_config(mhl_ctrl, 0);
+		mhl_ctrl->irq_req_done = false;
 	}
 
 	/* wait for i2c interrupt line to be activated */
@@ -1466,6 +1489,34 @@ static int mhl_sii_reg_config(struct i2c_client *client, bool enable)
 	int rc = -EINVAL;
 
 	pr_debug("%s\n", __func__);
+
+	if (!enable) {
+		if (reg_8941_vdda) {
+			regulator_disable(reg_8941_vdda);
+			regulator_put(reg_8941_vdda);
+			reg_8941_vdda = NULL;
+		}
+
+		if (reg_8941_smps3a) {
+			regulator_disable(reg_8941_smps3a);
+			regulator_put(reg_8941_smps3a);
+			reg_8941_smps3a = NULL;
+		}
+
+		if (reg_8941_l02) {
+			regulator_disable(reg_8941_l02);
+			regulator_put(reg_8941_l02);
+			reg_8941_l02 = NULL;
+		}
+
+		if (reg_8941_l24) {
+			regulator_disable(reg_8941_l24);
+			regulator_put(reg_8941_l24);
+			reg_8941_l24 = NULL;
+		}
+		return 0;
+	}
+
 	if (!reg_8941_l24) {
 		reg_8941_l24 = regulator_get(&client->dev,
 			"avcc_18");
